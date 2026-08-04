dea1482e9d2850797d682f535f2dfec7517c715b
Abubakar Shaik <abu9tqtd@gmail.com>
2026-08-05 09:09:17 +1300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 async function geminiAI(prompt) {
   try {
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash"
+      model: "gemini-1.5-flash"
     });
 
     const result = await model.generateContent(prompt);
